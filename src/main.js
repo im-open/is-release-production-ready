ae2c9e384b0e8e5f63bb95fa630d6c4ae2136437
Danielle Adams <danielle.adams3@extendhealth.com>
2022-10-31 12:23:09 -0600

    ARCH-1619 - Cleanup tech debt & old patterns
    - Recompile using npm 8.15.0 and node v16.17.1
    - Update actions/github package to latest version
    - Verify required args in action.yml are handled correctly in action code
    - Change instances of === 'true' to core.getBooleanInput()
    - Update the octokit await calls to use the await octokit.action.then().catch() pattern instead of a mix of that and try/catches.

--- src/main.js
+++ src/main.js
@@ -9,37 +9,40 @@ const requiredArgOptions = {
 
 const token = core.getInput('token', requiredArgOptions);
 const releaseTag = core.getInput('release-tag', requiredArgOptions);
-const failForPrerelease = core.getInput('fail-for-prerelease') == 'true';
+const failForPrerelease = core.getBooleanInput('fail-for-prerelease');
 
 const octokit = github.getOctokit(token);
-let loggingFunction = failForPrerelease ? core.setFailed : core.info;
+const loggingFunction = failForPrerelease ? core.setFailed : core.info;
 
 async function run() {
-  try {
-    core.info('Checking production readiness...');
+  core.info('Checking production readiness...');
 
-    // Currently the API only returns published releases (not draft) so the error
-    // condition will be triggered for drafts and when releases are not found.
-    const response = await octokit.rest.repos.getReleaseByTag({
+  // Currently the API only returns published releases (not draft) so the error
+  // condition will be triggered for drafts and when releases are not found.
+  await octokit.rest.repos
+    .getReleaseByTag({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       tag: releaseTag
-    });
+    })
+    .then(response => {
+      const draft = response.data.draft;
+      const prerelease = response.data.prerelease;
 
-    const draft = response.data.draft;
-    const prerelease = response.data.prerelease;
-
-    if (!draft && !prerelease) {
-      core.info('The release is production ready.');
-      core.setOutput('PRODUCTION_READY', true);
-    } else {
-      loggingFunction('The release is not production ready, it is marked as a pre-release');
+      if (!draft && !prerelease) {
+        core.info('The release is production ready.');
+        core.setOutput('PRODUCTION_READY', true);
+      } else {
+        loggingFunction('The release is not production ready, it is marked as a pre-release');
+        core.setOutput('PRODUCTION_READY', false);
+      }
+    })
+    .catch(() => {
+      loggingFunction(
+        `The release '${releaseTag}' is not production ready.  It was either not found or it was a draft release.`
+      );
       core.setOutput('PRODUCTION_READY', false);
-    }
-  } catch (error) {
-    loggingFunction(`The release '${releaseTag}' is not production ready, it was either not found or was a draft release.`);
-    core.setOutput('PRODUCTION_READY', false);
-  }
+    });
 }
 
 run();
